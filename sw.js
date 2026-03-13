70abbb975835fa6e170dc2eb25490c598b4760f0
Pablo Settimini <pablo@settimini.net>
2026-03-13 20:33:27 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'gastos-pwa-v2.0.0';
+const CACHE_VERSION = 'gastos-pwa-v2.0.1';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const APP_SHELL = [
